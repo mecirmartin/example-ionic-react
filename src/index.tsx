c6c880b20fc19326e59df8713452bf416966d348
mecirmartin <55835379+mecirmartin@users.noreply.github.com>
2021-03-02 07:55:12 +0100

    Reverted window.__deps change

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-//@ts-ignore
-if(!window.__skip_render){ 
-  ReactDOM.render(<App />, document.getElementById("root"));
-}
+
+ReactDOM.render(<App />, document.getElementById("root"));
